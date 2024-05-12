d6f3558dc8cf15ddec21354cfbdcd41e398b70e0
Gabriel Pavan <gabrielpavan2014@outlook.com>
2024-05-12 19:30:07 -0300

    Fix Layout login

--- app/src/main/java/com/example/splitworld/LoginActivity.java
+++ app/src/main/java/com/example/splitworld/LoginActivity.java
@@ -2,24 +2,12 @@ package com.example.splitworld;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.viewpager.widget.ViewPager;
 
 public class LoginActivity extends AppCompatActivity {
-
-    private ViewPager viewPager;
-    private int[] images = {R.drawable.malta, R.drawable.tenerife};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        viewPager = findViewById(R.id.viewPager);
-        ImagePagerAdapter adapter = new ImagePagerAdapter(this, images);
-        viewPager.setAdapter(adapter);
+        setContentView(R.layout.activity_login);
     }
 }
