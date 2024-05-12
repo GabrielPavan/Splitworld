2f9abda42e0d209ac152fe8ac2ae6a8822418a10
Gabriel Pavan <gabrielpavan2014@outlook.com>
2024-05-12 15:34:09 -0300

    background images

---
+++ app/src/main/java/com/example/splitworld/ImagePagerAdapter.java
@@ -0,0 +1,47 @@
+package com.example.splitworld;
+
+import androidx.viewpager.widget.PagerAdapter;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class ImagePagerAdapter extends PagerAdapter {
+
+    private Context context;
+    private int[] images;
+
+    public ImagePagerAdapter(Context context, int[] images) {
+        this.context = context;
+        this.images = images;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.item_image, container, false);
+
+        ImageView imageView = view.findViewById(R.id.imageView);
+        imageView.setImageResource(images[position]);
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+}
--- app/src/main/java/com/example/splitworld/LoginActivity.java
+++ app/src/main/java/com/example/splitworld/LoginActivity.java
@@ -7,18 +7,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.ViewPager;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private ViewPager viewPager;
+    private int[] images = {R.drawable.malta, R.drawable.tenerife};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.imageView), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        viewPager = findViewById(R.id.viewPager);
+        ImagePagerAdapter adapter = new ImagePagerAdapter(this, images);
+        viewPager.setAdapter(adapter);
     }
 }
